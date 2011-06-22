8b1078b2d21d5020c95395974c8197170cfe5afe
MakeToLearn <jordan@aace.org>
2011-06-22 14:32:35 -0400

    has to be a tuple

--- safehtmlform/fields.py
+++ safehtmlform/fields.py
@@ -9,7 +9,7 @@ class SafeHTMLField(forms.CharField):
         self.acceptable_elements = acceptable_elements
         additional_elements = kwargs.pop("additional_elements", None)
         if additional_elements:
-            self.acceptable_elements += additional_elements
+            self.acceptable_elements += tuple(additional_elements)
         self.help_text=_("This field allows the use of the following HTML tags: %(tags)s." % {'tags': self.acceptable_elements})
         super(SafeHTMLField, self).__init__(*args, **kwargs)
 
